9f798deb627bb3189174941b1628f6005d647bca
Dung Nguyen <dung34nguyen@gmail.com>
2020-06-11 23:51:16 -0500

    finish getting to punching site

--- clock_bot.py
+++ clock_bot.py
@@ -11,12 +11,21 @@ class clockBot():
         cougarnet_in.send_keys('dtngu212')
         pass_in.send_keys('Dethuong11@')
         login_btn.click()
-    def clockIn(self):
+    def get_to_punching_site(self):
         #wait for page to load 
         sleep(3)
         hr_icon=self.driver.find_element_by_xpath('//*[@id="portal_area"]/div[2]/div/div[2]/div[4]/a/div[1]/img')
+        window_before=self.driver.window_handles[0]
         hr_icon.click()
+        #because there is a new window pop up after clicking the HR icon
+        #you need to switch window before search for the time icon
+        window_after=self.driver.window_handles[1]
+        self.driver.switch_to.window(window_after)
+        time_icon=bot.driver.find_element_by_xpath('//*[@id="win0divPTNUI_LAND_REC_GROUPLET$0"]')
+        time_icon.click()
+
+    def clock_in(self):
         
 bot = clockBot()
 bot.login()
-bot.clockIn()
+bot.get_to_punching_site()
